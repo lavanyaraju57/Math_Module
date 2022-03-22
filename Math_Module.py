c8b9b8137e41411218a5f8089cda233667cfa6de
lavanyaraju57 <99262310+lavanyaraju57@users.noreply.github.com>
2022-03-22 20:42:44 +0530

    Add files via upload

---
+++ Math_Module.py
@@ -0,0 +1,143 @@
+import math
+
+'''CEIL'''
+
+# print(math.ceil(5.1))
+# print(math.ceil(7.3))
+# print(math.ceil(1.9))
+# print(math.ceil(6.8))
+# print(math.ceil(-9.2))
+# print(math.ceil(0.7))
+# print(math.ceil(45.0))
+# print(math.ceil(13.8))
+
+
+'''EXP'''
+
+# print(math.exp(9))
+# print(math.exp(36))
+# print(math.exp(0))
+# print(math.exp(1))
+# print(math.exp(14))
+
+
+'''FACTORIAL'''
+
+# print(math.factorial(7))
+# print(math.factorial(13))
+# print(math.factorial(0))
+# print(math.factorial(1))
+# print(math.factorial(2))
+# print(math.factorial(3))
+# print(math.factorial(26))
+
+
+'''FLOOR'''
+
+# print(math.floor(4))
+# print(math.floor(6.9))
+# print(math.floor(32.5))
+# print(math.floor(16.2))
+# print(math.floor(-42.7))
+# print(math.floor(-15.3))
+
+
+'''POW'''
+
+# s=math.pow(5,8)
+# f=7**2
+# print(s,type(s))
+# print(f,type(f))
+
+
+'''PI'''
+
+# print(math.pi)
+# print(math.pi*5)
+# print(math.pi*2**7)
+# print(math.pi*-9**4)
+# print(math.pi*24.6)
+
+
+'''SQRT'''
+
+# print(math.sqrt(6))
+# print(math.sqrt(12.8))
+# print(math.sqrt(37))
+# print(math.sqrt(15.24))
+
+
+'''ISQRT'''
+
+# t=7
+# s=25
+# m=2
+# p=0
+# print(math.isqrt(t))
+# print(math.isqrt(M))
+# print(math.isqrt(s))
+# print(math.isqrt(p))
+
+
+'''FSUM'''
+
+# n=[1,2,3,4,5]
+# print(sum(n))
+# print(math.fsum(n))
+# print(int(math.fsum(n)))
+
+
+'''PROD'''
+
+# s=[5,8,9,3,4]
+# print(math.prod(s))
+
+
+'''FMOD'''
+
+# m=13%5
+# print(m)
+# print(math.fmod(13,5))
+
+
+'''FABS'''
+
+# v=-8
+# print(abs(v))
+# print(math.fabs(v))
+
+
+'''SIN, COS, TAN'''
+
+# print(math.cos(45))
+# print(math.sin(45))
+# print(math.tan(45))
+
+
+'''TRUNC'''
+
+# print(math.trunc(7.33))
+# print(math.trunc(-2.88))
+# print(math.trunc(52.6))
+# print(math.trunc(-23.9))
+
+
+'''GCD'''
+
+# print(math.gcd(4,9))
+# print(math.gcd(15,11))
+# print(math.gcd(15,13))
+# print(math.gcd(24,32))
+# print(math.gcd(52,86))
+
+
+'''LCM'''
+
+# print(math.lcm(8,2))
+# print(math.lcm(26,7))
+# print(math.lcm(42,24))
+
+
+
+# import math
+# print(dir(math))
